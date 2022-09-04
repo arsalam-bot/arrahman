20eaea48907b62a138e4123ae99bda6a48998744
Arsalam <launualam@gmail.com>
2022-09-04 22:05:15 +0700

    Add: HMVC Auth
    Add HMVC structure for Auth

---
+++ app/Modules/Auth/Controllers/Auth.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modules\Auth\Controllers;
+
+use App\Modules\Auth\Controllers\BaseController;
+
+class Auth extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
